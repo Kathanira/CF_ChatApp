0763dccf2f34302928706c3e1ace0d7ff6e10f72
Kathanira <112494140+Kathanira@users.noreply.github.com>
2023-01-09 12:27:40 +0100

    Updated Chat.js

--- components/Chat.js
+++ components/Chat.js
@@ -1,64 +1,71 @@
-import React, { Component } from "react";
-import { View, Platform, KeyboardAvoidingView, Text, TouchableOpacity, StyleSheet } from "react-native";
-import { GiftedChat } from 'react-native-gifted-chat'
+import React from 'react';
+ import { StyleSheet, View, Platform, KeyboardAvoidingView } from 'react-native';
+ import { GiftedChat, Bubble } from 'react-native-gifted-chat';
 
-export class Chat extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      messages: [],
-    }
-  }
-componentDidMount() {
-     const name = this.props.route.params.name;
+ export default class Chat extends React.Component {
+   constructor() {
+     super();
+     this.state = {
+       messages: [],
+     };
+   }
+
+   componentDidMount() {
+     // Set the name property to be included in the navigation bar
+     let name = this.props.route.params.name;
+
+     this.setState({
+       messages: [
+         {
+           _id: 1,
+           text: `Hi ${name}, how are you today?`,
+           createdAt: new Date(),
+           user: {
+             _id: 2,
+             name: 'React Native',
+             avatar: 'https://placeimg.com/140/140/any',
+           },
+         },
+         {
+           _id: 2,
+           text: `${name} has entered the chat`,
+           createdAt: new Date(),
+           system: true,
+         },
+       ],
+     });
+
      this.props.navigation.setOptions({ title: name });
-  
-      this.setState({
-        messages: [
-          {
-            _id: 1,
-            text: 'Hello developer',
-            createdAt: new Date(),
-            user: {
-              _id: 2,
-              name: 'React Native',
-              avatar: 'https://placeimg.com/140/140/any',
-            },
-           },
-           {
-            _id: 2,
-            text: 'This is a system message',
-            createdAt: new Date(),
-            system: true,
-           },
-        ],
-      });
-    }
-    onSend(messages = []) {
-      this.setState(previousState => ({
-        messages: GiftedChat.append(previousState.messages, messages),
-      }));
-    }
-    
- //Bubble customization
- renderBubble(props) {
-  return (
-    <Bubble
-      {...props}
-      wrapperStyle={{
-        right: {
-          backgroundColor: '#000',
-        },
-      }}
-    />
-  );
-}
+   }
 
-render() {
-     const { color } = this.props.route.params;
+   onSend(messages = []) {
+     this.setState((previousState) => ({
+       messages: GiftedChat.append(previousState.messages, messages),
+     }));
+   }
+
+   renderBubble(props) {
      return (
-       <View style={{ flex: 1, backgroundColor: color }}>
-          <GiftedChat
+       <Bubble
+         {...props}
+         wrapperStyle={{
+           right: {
+             backgroundColor: '#000',
+           },
+           left: {
+             backgroundColor: '#fff',
+           },
+         }}
+       />
+     );
+   }
+
+   render() {
+     // Set the color property as background color for the chat screen
+     let color = this.props.route.params.color;
+     return (
+       <View style={[styles.container, { backgroundColor: color }]}>
+         <GiftedChat
            renderBubble={this.renderBubble.bind(this)}
            messages={this.state.messages}
            onSend={(messages) => this.onSend(messages)}
@@ -66,12 +73,16 @@ render() {
              _id: 1,
            }}
          />
-         { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
+         {Platform.OS === 'android' ? (
+           <KeyboardAvoidingView behavior="height" />
+         ) : null}
        </View>
      );
    }
  }
 
  const styles = StyleSheet.create({
-   chatContainer: { flex: 1, justifyContent: "center", alignItems: "center" },
+   container: {
+     flex: 1,
+   },
  });
